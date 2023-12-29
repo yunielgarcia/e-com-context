967fc41e931a585f784faa1a14bb52dd88c82043
yggarcia <garciayuni84@gmail.com>
2023-12-29 18:48:08 -0500

    End project with useReducer

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header.jsx';
+import Shop from './components/Shop.jsx';
+import CartContextProvider, {ShoppingCartContext} from "./store/shopping-cart-context.jsx";
+
+function App() {
+    return (
+        <CartContextProvider>
+            <Header/>
+            <Shop/>
+        </CartContextProvider>
+    );
+}
+
+export default App;
---
+++ src/components/CartModal.jsx
@@ -0,0 +1,32 @@
+import {forwardRef, useContext, useImperativeHandle, useRef} from 'react';
+import { createPortal } from 'react-dom';
+import Cart from './Cart';
+import {ShoppingCartContext} from "../store/shopping-cart-context.jsx";
+
+const CartModal = forwardRef(function Modal(
+  { title, actions },
+  ref
+) {
+  const dialog = useRef();
+
+    useImperativeHandle(ref, () => {
+    return {
+      open: () => {
+        dialog.current.showModal();
+      },
+    };
+  });
+
+  return createPortal(
+    <dialog id="modal" ref={dialog}>
+      <h2>{title}</h2>
+      <Cart />
+      <form method="dialog" id="modal-actions">
+        {actions}
+      </form>
+    </dialog>,
+    document.getElementById('modal')
+  );
+});
+
+export default CartModal;
